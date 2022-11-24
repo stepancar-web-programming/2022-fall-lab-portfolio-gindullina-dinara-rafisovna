01f3ddb0dd2b30d7e0eac38822b12ba78accc44f
blabla9111 <dinaragindullinaa@gmail.com>
2022-11-24 20:21:24 +0300

    got rid of JQuery

--- src/script.js
+++ src/script.js
@@ -3,20 +3,19 @@ const index = 0;
 let textPosition = 0;
 let flag = true;
 const button = document.getElementById('Infobutton');
+let openPopup = document.getElementsByClassName("open-popup")[0];
+let popupContainer = document.getElementsByClassName("popup-container")[0];
+const closeButton = document.getElementsByClassName("close-button")[0];
 
 document.getElementById('greeting-text').textContent = 'Гиндуллина Динара Рафисовна'; // просто было интересно так вставить текст
 
-$('#infoButton').click(() => {
-  window.location.href = 'index.html';
-  return false;
-});
 function typewriter() {
   if (flag) {
     quoteArray[index] += ' ';
     flag = false;
   }
-
-  document.querySelector('#quote').innerHTML = `${quoteArray[index].substring(0, textPosition)}<span>\u25AE</span>`;
+  text = quoteArray[index].substring(0, textPosition)
+  document.querySelector('#quote').innerHTML =text+'<span>\u25AE</span>';
   textPosition += 1;
   if (textPosition !== quoteArray[index].length) {
     setTimeout(() => {
@@ -31,12 +30,12 @@ function typewriter() {
   }
 }
 
-function popup() {
-  $('.open-popup').on('click', () => {
-    $('.popup-container').css('display', 'flex');
+function popup(){
+  openPopup.addEventListener('click',()=>{
+    popupContainer.style.display = 'flex'
   });
-  $('.close-button').on('click', () => {
-    $('.popup-container').css('display', 'none');
+  closeButton.addEventListener('click',()=>{
+    popupContainer.style.display = 'none'
   });
 }
 
